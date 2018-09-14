45e98184cc47ce835d284591af5e15c30b715687
Chris Teodorski <chris.teodorski@gmail.com>
2018-09-13 22:07:02 -0400

    I love crashplan

---
+++ twitterparse.py
@@ -0,0 +1,19 @@
+import pandas
+from datetime import datetime
+from collections import Counter
+twitterPosts = []
+
+fieldnames = ['favorite_count','source,text','in_reply_to_screen_name','is_retweet','created_at','retweet_count','id_str']
+csvfile = pandas.read_csv('twitter.csv', names=fieldnames)
+dates = csvfile.created_at.tolist()
+for date in dates:
+	date = date.replace(" +0000","")
+	datetime_object = datetime.strptime(date,'%a %b %d %H:%M:%S %Y')
+ 	formateddate = datetime_object.strftime("%Y-%m-%d")
+	twitterPosts.append(formateddate)
+	
+post_count = Counter(twitterPosts)
+
+for date in sorted(post_count):
+	print date, post_count[date]
+
